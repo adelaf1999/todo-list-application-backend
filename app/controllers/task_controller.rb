bf2ab99eabec31b898ced88c962ea4c0dcbf1b8b
Adel Abou Fakher <adelwaboufakher@gmail.com>
2022-10-07 09:05:17 -0500

    Create action in task controller complete

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,2 +1,28 @@
 class TaskController < ApplicationController
+
+  def create
+
+    text = params[:text]
+
+    task_was_created = Task.create_task(text)
+
+    if task_was_created
+
+      @success = true
+
+      @message = 'Successfully created task'
+
+      @tasks = Task.get_all_tasks
+
+
+    else
+
+      @success = false
+
+      @message = 'There was an error creating the task'
+
+    end
+
+  end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,39 @@
 class Task < ApplicationRecord
+
+  def self.create_task(text)
+
+    if text.blank?
+
+      false
+
+    else
+
+      text = text.strip
+
+      task = Task.create!(text: text)
+
+      task.nil? ? false : true
+
+    end
+
+
+  end
+
+  def self.get_all_tasks
+
+    tasks = []
+
+    Task.all.each do |task|
+
+      tasks.push({
+                    id: task.id,
+                    text: task.text
+                 })
+
+    end
+
+    tasks
+
+  end
+
 end
